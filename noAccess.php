30fc12f59aa9363f71bdaf309ab911652865ef06
khadija <56749070+khadijakumar@users.noreply.github.com>
2021-08-17 11:01:09 +0200

    task
    Task

---
+++ noAccess.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="site.css">
+  <style>
+  </style>
+ </head>
+ <body>
+  <div class="alert">
+    <section class="section">
+      <h2><strong>Access Denied!</strong><br> This page is only for Admin.<br> Please return to the <a class="Na" href="account.php">Home</a> page</h2>
+    </section>
+  </div>
+ </body>
+</html>
